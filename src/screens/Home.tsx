07a959c76896d73931cc3dc0164687c92045a944
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-08-21 16:36:46 +0530

    feat created copy to clipboard functionality

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
+import { FaCheck } from "react-icons/fa";
+import { GoCopy } from "react-icons/go";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  const handleCopyToClipborad = () => {};
+  const [isCopiedToClipBoard, setIsCopyToClipBoard] = useState(false);
+
+  const handleCopyToClipborad = () => {
+    setIsCopyToClipBoard(true);
+    navigator.clipboard.writeText(
+      "https://github.com/meabhipatel/small_react_app.git"
+    );
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <button
+          onClick={handleCopyToClipborad}
+          className="fixed left-0 top-0 flex w-full justify-center items-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+        >
           Get started by clone&nbsp;
-          <code className="font-mono font-bold">from github/meabhipatel</code>
-        </p>
+          <code className="font-mono font-bold mr-3">
+            from github/meabhipatel
+          </code>
+          {isCopiedToClipBoard ? (
+            <FaCheck className="text-xl  text-green-500/70" />
+          ) : (
+            <GoCopy className="text-xl stroke-1 text-gray-300/50" />
+          )}
+        </button>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
